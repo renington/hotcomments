7a024327e428c624b4706159f6e39f03f9e27b72
Renington Neri <renington.neri@gmail.com>
2017-12-11 23:39:14 -0200

    feat: creating new comment and comments stateless components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import 'bootstrap-css-only'
 
+import NewComment from './NewComment'
+import Comments from './Comments'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hot Comments</h1>
-          <p><button className="btn btn-primary">CLICK HERE</button> </p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <NewComment />
+        <Comments />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/NewComment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewComment = () => {
+    return (
+        <div className="row">
+            <textarea placeholder="Send Comment" className="form-control"></textarea>
+        </div>
+    )
+}
+
+export default NewComment
