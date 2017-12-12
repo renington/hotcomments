8f549c23801c8662d4c65735418fc65407956003
Renington Neri <renington.neri@gmail.com>
2017-12-11 23:31:53 -0200

    lib: Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import 'bootstrap-css-only'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hot Comments</h1>
+          <p><button className="btn btn-primary">CLICK HERE</button> </p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
